e891a59bef2067f90d2202dcca40cabe24827a26
Nico <nicoburkart@web.de>
2021-08-27 14:44:44 +0200

    style-selection-modal

---
+++ src/app/modules/order/components/style-selection-modal/style-selection-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'order-style-selection-modal',
+  templateUrl: './style-selection-modal.component.html',
+  styleUrls: ['./style-selection-modal.component.scss'],
+})
+export class StyleSelectionModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  closeModal() {}
+}
